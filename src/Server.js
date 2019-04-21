a059b9f436df075473c8ab0024236d2f38569bf1
Your Name <you@example.com>
2019-04-21 14:57:15 -0300

    server

--- src/Server.js
+++ src/Server.js
@@ -1,38 +1,35 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const path = require('path')
-const cors = require('cors')
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 io.on('connection', socket => {
     socket.on('connectRoom', box => {
-        socket.join(box)
-    })
-})
+        socket.join(box);
+    });
+});
 
-mongoose.connect(
-    "mongodb+srv://rbmoraes:rbmoraes@boxjs-wcxyw.mongodb.net/boxjs?retryWrites=true",
-    {
-        useNewUrlParser: true
-    }
-);
+mongoose.connect('mongodb+srv://nicholas:giudice@cluster0-eqjfk.mongodb.net/test?retryWrites=true', {
+    useNewUrlParser: true
+});
 
 app.use((req, res, next) => {
-    req.io = io
+    req.io = io;
 
-    return next()
-})
+    return next();
+});
 
-app.use(express.json()) //ajuda a entender as requisições que vem em formato json
-app.use(express.urlencoded({ extended: true })) // permite que envie arquivos nas requesições
-app.use("/files", express.static(path.resolve(__dirname, "..", "tmp"))) //toda vez que acessar o diretório Files, ele redireciona os arquivos fisicos da pasta tmp
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use('/files', express.static(path.resolve(__dirname, '..', 'temp')));
 
-app.use(require('./routes')) // aponta (importa) o arquivo onde está configurado as rotas
+app.use(require('./routes'));
 
-server.listen(process.env.PORT || '3333') // habilita uma porta para sua aplicação rodar
+server.listen(process.env.PORT || '3000');
