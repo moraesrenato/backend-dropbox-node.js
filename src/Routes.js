cd81e061d626268ccc422562e141c2e409b46609
Your Name <you@example.com>
2019-04-21 15:06:37 -0300

    routes atualizada

--- src/Routes.js
+++ src/Routes.js
@@ -1,16 +1,14 @@
 const express = require('express')
 const multer = require('multer')
-const multerConfig = require('./config/Multer')
-const routes = express.Router()    // com isso a parte de rotas está separada do módulo principal
+const multerConfig = require('./config/multer')
 
+const routes = express.Router();
 
-const BoxController = require('./controllers/BoxController')
-const FileController = require('./controllers/FileController')
+const BoxController = require('./controllers/BoxController');
+const FileController = require('./controllers/FileController');
 
-routes.post('/boxes', BoxController.store)
+routes.post('/boxes', BoxController.store);
+routes.get('/boxes/:id', BoxController.show);
+routes.post('/boxes/:id/files', multer(multerConfig).single('file'), FileController.store);
 
-routes.get('/boxes/:id', BoxController.show)
-
-routes.post('/boxes/:id/file', multer(multerConfig).single('file'), FileController.store)
-
-module.exports = routes  // com isso ele pode exportar alguma informação do arquivo.
+module.exports = routes;
